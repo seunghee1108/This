94db2b27509ffbf78a0aa7f410fd8a5b4d1f9822
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-12 22:02:18 +0900

    arrow함수 작성

---
+++ arrow.js
@@ -0,0 +1,11 @@
+function outerOne(a) {
+   const innerOne = b => console.log(b);
+   innerOne(a);
+  }
+console.log(outerOne("hello"))
+
+fs.readFile("./html", data, (error) => {
+
+})
+
+// 두개 결과가 똑같이 나옴
